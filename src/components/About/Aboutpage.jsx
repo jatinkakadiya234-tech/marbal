2ef641f6a0254ef265521f2cd6158f2280a02d21
jatinkakadiya <jatinkakadiya234@gmail.com>
2025-12-08 15:35:38 +0530

    dsddsvs

--- src/components/About/Aboutpage.jsx
+++ src/components/About/Aboutpage.jsx
@@ -8,7 +8,7 @@ export default  function About () {
         <>
         {/* <Banner /> */}
         <div className="bg-gradient-to-r from-[#0E5543] to-[#0E5543] py-6 md:py-16 text-white text-center px-4">
-            <p className="text-2xl md:text-4xl font-bold mb-2 md:mb-4 mt-10 sm:mt-12">About Us</p>
+            <p className="text-2xl md:text-4xl font-bold mb-2 md:mb-4 mt-20 sm:mt-12">About Us</p>
             <p className="text-base md:text-xl opacity-90">Discover Our Journey in Stone Excellence - 25+ Years of Crafting Premium Marble, Granite & Sandstone</p>
         </div>
         <ShowcasingTimeline/>
